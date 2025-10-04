7370353c7f17d4b175307232d90de61ced2a675d
Lumilu99 <yenwen_lu@hotmail.com>
2025-10-04 18:25:20 +0800

    Feat: With hook keypress, ESC now works, can press x to close the program

--- includes/cub3d.h
+++ includes/cub3d.h
@@ -6,7 +6,7 @@
 /*   By: yelu <yelu@student.42kl.edu.my>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/04 14:56:07 by yelu              #+#    #+#             */
-/*   Updated: 2025/10/04 15:02:37 by yelu             ###   ########.fr       */
+/*   Updated: 2025/10/04 17:55:27 by yelu             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,7 @@
 # define CUB3D_H
 
 # include "../mlx/mlx.h"
+# include "../libft/libft.h"
 # include <X11/keysym.h>
 # include <X11/X.h>
 # include <stdlib.h>
@@ -22,6 +23,14 @@ typedef struct s_data
 {
 	void	*mlx_ptr;
 	void	*win_ptr;
+	char	map[6][6];
 }	t_data;
+
+int	ft_close(t_data *data);
+int	on_keypress(int keysym, t_data *data);
+void draw_minimap(t_data *data, void *mlx, void *win);
+int draw_square(void *mlx, void *win, int x, int y, int size, int color);
+
+
 
 #endif
--- src/main.c
+++ src/main.c
@@ -6,22 +6,65 @@
 /*   By: yelu <yelu@student.42kl.edu.my>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/04 14:55:26 by yelu              #+#    #+#             */
-/*   Updated: 2025/10/04 15:03:16 by yelu             ###   ########.fr       */
+/*   Updated: 2025/10/04 18:07:19 by yelu             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "cub3d.h"
+#include "../includes/cub3d.h"
 
-int main()
+void	map_init(t_data *data)
+{
+	ft_bzero(data, sizeof(t_data));
+	char *map_init[6] = 
+	{
+		"11111",
+		"10001",
+		"10001",
+		"10001",
+		"11111",
+		""
+	};
+	for (int i = 0; i < 6; i++)
+		strcpy(data->map[i], map_init[i]);
+}
+
+int	on_keypress(int keysym, t_data *data)
+{
+	if (keysym == XK_Escape)
+		ft_close(data);
+	return (0);
+}
+
+int	ft_close(t_data *data)
+{
+	mlx_destroy_window(data->mlx_ptr, data->win_ptr);
+	mlx_destroy_display(data->mlx_ptr);
+	free(data->mlx_ptr);
+	exit(0);
+}
+
+int main(int argc, char **argv)
 {
 	t_data	data;
 
-    data.mlx_ptr = mlx_init();
-	if (!data.mlx_ptr)
-		return (1);
-    data.win_ptr = mlx_new_window(data.mlx_ptr, 800, 600, "Cub3D");
-	if (!data.win_ptr)
-		return (1);
-    mlx_loop(data.mlx_ptr);
-    return 0;
+	(void)argv;
+	if (argc == 2)
+	{
+		map_init(&data);
+		data.mlx_ptr = mlx_init();
+		if (!data.mlx_ptr)
+			return (1);
+		data.win_ptr = mlx_new_window(data.mlx_ptr, 800, 600, "Cub3D");
+		if (!data.win_ptr)
+			return (1);
+		mlx_hook(data.win_ptr, KeyPress, KeyPressMask, on_keypress, &data);
+		mlx_hook(data.win_ptr, DestroyNotify, SubstructureNotifyMask,
+			ft_close, &data);
+		mlx_loop(data.mlx_ptr);
+	}
+	else
+	{
+		ft_putstr_fd("Error\nUsage: ./cub3D <map file.ber>", 2);
+		exit(1);
+	}
 }
