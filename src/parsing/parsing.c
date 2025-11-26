ca28052a9bab5da4f7f2a40054ad659f6b2b145a
wyshee14 <weiyee1401@gmail.com>
2025-11-26 21:10:02 +0800

    add filename checking

--- src/parsing/parsing.c
+++ src/parsing/parsing.c
@@ -3,10 +3,30 @@
 /*                                                        :::      ::::::::   */
 /*   parsing.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: yelu <yelu@student.42kl.edu.my>            +#+  +:+       +#+        */
+/*   By: wshee <wshee@student.42kl.edu.my>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/10/14 15:36:19 by yelu              #+#    #+#             */
-/*   Updated: 2025/10/14 15:36:19 by yelu             ###   ########.fr       */
+/*   Created: 2025/11/26 20:28:09 by wshee             #+#    #+#             */
+/*   Updated: 2025/11/26 21:05:17 by wshee            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+# include "stdbool.h"
+# include "stdio.h"
+
+bool check_file_ext(const char *filename)
+{
+	if (filename[0] == '\0')
+		printf("Invalid map");
+	if (*filename++)
+		printf("%c", *filename);
+	return true;
+}
+
+int main(int ac, char **av)
+{
+	if (ac != 2)
+		printf("Number of arguments must be 2!");
+	if(check_file_ext(av[1]))
+		printf("Invalid map");
+	return 0;
+}
