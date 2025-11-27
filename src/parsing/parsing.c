0db7e1e99f3f0d1b4b0c276b630841ee4a18f20f
wyshee14 <weiyee1401@gmail.com>
2025-11-27 22:06:56 +0800

    added check file extension

--- src/parsing/parsing.c
+++ src/parsing/parsing.c
@@ -6,27 +6,44 @@
 /*   By: wshee <wshee@student.42kl.edu.my>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/26 20:28:09 by wshee             #+#    #+#             */
-/*   Updated: 2025/11/26 21:05:17 by wshee            ###   ########.fr       */
+/*   Updated: 2025/11/27 21:42:40 by wshee            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-# include "stdbool.h"
-# include "stdio.h"
+# include "../../includes/cub3d.h"
+# include <stdio.h>
 
 bool check_file_ext(const char *filename)
 {
+	const char *ext = ".cub";
+	size_t filename_len = ft_strlen(filename);
+	size_t ext_len = ft_strlen(ext);
+
 	if (filename[0] == '\0')
-		printf("Invalid map");
-	if (*filename++)
-		printf("%c", *filename);
+		return false;
+	if (filename_len == ext_len)
+		return false;
+	while (filename[filename_len - 1] && ext_len > 0)
+	{
+		if (filename[filename_len - 1] != ext[ext_len - 1])
+			return false;
+		filename_len--;
+		ext_len--;
+	}
 	return true;
 }
 
 int main(int ac, char **av)
 {
 	if (ac != 2)
-		printf("Number of arguments must be 2!");
-	if(check_file_ext(av[1]))
+	{
+		printf("Number of arguments must be 2");
+		return 1;
+	}
+	if(!check_file_ext(av[1]))
+	{
 		printf("Invalid map");
+		return 1;
+	}
 	return 0;
 }
