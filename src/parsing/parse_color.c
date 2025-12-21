094efc73fb0c8c812f06b1287d54e17e64f2cdc9
wyshee14 <weiyee1401@gmail.com>
2025-12-21 21:41:01 +0800

    added free_array

--- src/parsing/parse_color.c
+++ src/parsing/parse_color.c
@@ -6,7 +6,7 @@
 /*   By: wshee <wshee@student.42kl.edu.my>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/21 19:25:06 by wshee             #+#    #+#             */
-/*   Updated: 2025/12/21 19:29:02 by wshee            ###   ########.fr       */
+/*   Updated: 2025/12/21 20:10:31 by wshee            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,12 +43,16 @@ void free_2d_array(void **array)
 		free(array[i]);
 		i++;
 	}
+	free (array);
 }
 
 int parse_color(char *identifier, char *color)
 {
-	char **rgb = ft_split(color, ',');
-	int counter = 0;
+	char **rgb;
+	int counter;
+
+	rgb = ft_split(color, ',');
+	counter = 0;
 	while (rgb[counter])
 		counter++;
 	if (counter != 3)
@@ -56,6 +60,7 @@ int parse_color(char *identifier, char *color)
 	int r = convert_rgb_to_int(rgb[0]);
 	int g = convert_rgb_to_int(rgb[1]);
 	int b = convert_rgb_to_int(rgb[2]);
+	free(rgb);
 	// convert into hexa format 0xFFFFFF in integer form
 	if (r < 0 || r > 255 || g < 0 || g > 255 || b < 0 || b > 255)
 		return (-1);
