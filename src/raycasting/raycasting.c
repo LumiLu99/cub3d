da983ecc685985ea1efafffdc923ea11595b0eae
Lumilu99 <yenwen_lu@hotmail.com>
2026-01-03 00:56:34 +0800

    Refactor: Norminette done for raycasting.c

--- src/raycasting/raycasting.c
+++ src/raycasting/raycasting.c
@@ -6,31 +6,29 @@
 /*   By: yelu <yelu@student.42kl.edu.my>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/05 00:19:53 by yelu              #+#    #+#             */
-/*   Updated: 2025/12/31 16:27:19 by yelu             ###   ########.fr       */
+/*   Updated: 2026/01/03 00:16:29 by yelu             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/cub3d.h"
 
-static void ray_init(t_data *data, int x)
+static void	ray_init(t_data *data, int x)
 {
-    data->ray.camera_x = (2 * x) / (double)(WIDTH) - 1;
-    data->ray.ray_dir_x = data->player.dir_x + (data->player.plane_x
-		* data->ray.camera_x);
-    data->ray.ray_dir_y = data->player.dir_y + (data->player.plane_y
-		* data->ray.camera_x);
-    if (data->ray.ray_dir_x == 0)
-        data->ray.delta_x = 1e30;
-    else
-        data->ray.delta_x = fabs(1 / data->ray.ray_dir_x);
-
-    if (data->ray.ray_dir_y == 0)
-        data->ray.delta_y = 1e30;
-    else
-        data->ray.delta_y = fabs(1 / data->ray.ray_dir_y);
-
-    data->ray.map_x = (int)(data->player.pos_x);
-    data->ray.map_y = (int)(data->player.pos_y);
+	data->ray.camera_x = (2 * x) / (double)(WIDTH) - 1;
+	data->ray.ray_dir_x = data->player.dir_x + (data->player.plane_x
+			* data->ray.camera_x);
+	data->ray.ray_dir_y = data->player.dir_y + (data->player.plane_y
+			* data->ray.camera_x);
+	if (data->ray.ray_dir_x == 0)
+		data->ray.delta_x = 1e30;
+	else
+		data->ray.delta_x = fabs(1 / data->ray.ray_dir_x);
+	if (data->ray.ray_dir_y == 0)
+		data->ray.delta_y = 1e30;
+	else
+		data->ray.delta_y = fabs(1 / data->ray.ray_dir_y);
+	data->ray.map_x = (int)(data->player.pos_x);
+	data->ray.map_y = (int)(data->player.pos_y);
 }
 
 void	ray_tex_init(t_data *data)
@@ -47,8 +45,9 @@ void	ray_tex_init(t_data *data)
 void	draw_dda(t_data *data)
 {
 	int	x;
-	int side = -1;
+	int	side;
 
+	side = -1;
 	x = 0;
 	while (x < WIDTH)
 	{
--- src/raycasting/raycasting_utils2.c
+++ src/raycasting/raycasting_utils2.c
@@ -6,7 +6,7 @@
 /*   By: yelu <yelu@student.42kl.edu.my>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/02 23:10:37 by yelu              #+#    #+#             */
-/*   Updated: 2026/01/02 23:33:21 by yelu             ###   ########.fr       */
+/*   Updated: 2026/01/03 00:55:56 by yelu             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,11 @@
 
 void	ray_tex_sides(t_data *data, int side)
 {
-	data->ray.wall_x = data->player.pos_x + data->ray.perp_dist * data->ray.ray_dir_x;
+	data->ray.wall_x = data->player.pos_x + data->ray.perp_dist
+		* data->ray.ray_dir_x;
 	if (!side)
-		data->ray.wall_x = data->player.pos_y + data->ray.perp_dist * data->ray.ray_dir_y;
+		data->ray.wall_x = data->player.pos_y + data->ray.perp_dist
+			* data->ray.ray_dir_y;
 	data->ray.wall_x -= floor(data->ray.wall_x);
 	if (!side)
 	{
@@ -32,24 +34,26 @@ void	ray_tex_sides(t_data *data, int side)
 		else
 			data->ray.side = NORTH;
 	}
-	data->ray.tex_num = (int)(data->ray.wall_x * (double)(data->tex[data->ray.side].img_width));
-	if ((!side && data->ray.ray_dir_x > 0)
-		|| (side && data->ray.ray_dir_y < 0))
-		data->ray.tex_num = data->tex[data->ray.side].img_width - data->ray.tex_num - 1;
+	data->ray.tex_num = (int)(data->ray.wall_x
+			* (double)(data->tex[data->ray.side].img_width));
+	if ((!side && data->ray.ray_dir_x > 0) || (side && data->ray.ray_dir_y < 0))
+		data->ray.tex_num = data->tex[data->ray.side].img_width
+			- data->ray.tex_num - 1;
 }
 
-void    ray_tex_wall(t_data *data, int x, int y)
+void	ray_tex_wall(t_data *data, int x, int y)
 {
-	int wall_step;
-	int	tex_y;
+	int				wall_step;
+	int				tex_y;
 	unsigned int	color;
 
 	wall_step = y * 2 - HEIGHT + data->ray.wall_height;
 	tex_y = (wall_step * data->tex[data->ray.side].img_height)
 		/ (2 * data->ray.wall_height);
 	color = *((unsigned int *)(data->tex[data->ray.side].addr
-			+ (tex_y * data->tex[data->ray.side].line_len
-				+ data->ray.tex_num * (data->tex[data->ray.side].bits_per_pixel / 8))));
+				+ (tex_y * data->tex[data->ray.side].line_len
+					+ data->ray.tex_num
+					* (data->tex[data->ray.side].bits_per_pixel / 8))));
 	my_mlx_pixel_put(data, x, y, color);
 }
 
